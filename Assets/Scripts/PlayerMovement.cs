2ac069bb7abb045bce8694b9edaf704f4fdaff96
Marius Vainauskis <Marius.Vainauskis@ktu.edu>
2024-02-08 17:56:51 +0200

    Foundations of the game.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Unity characteristics
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    [SerializeField] private LayerMask jumpableGround;
+
+    // Parameters
+    private float dirX = 0f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 10f;
+
+    private enum MovementState { idle, running, jumping, falling }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+        // Player jumps
+        if (Input.GetButtonDown("Jump") && isGrounded())
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+
+        updateMovementState();
+    }
+
+    // Updates player's movement state
+    private void updateMovementState()
+    {
+        MovementState state;
+
+        // Does the player run?
+        if (dirX > 0f) // running to the right (->)
+        {
+            state = MovementState.running;
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f) // running to the left (<-)
+        {
+            state = MovementState.running;
+            sprite.flipX = true;
+        }
+        else // standing still
+            state = MovementState.idle;
+
+        if (rb.velocity.y > .1f)
+            state = MovementState.jumping; // moving up with speed > .1f
+        else if (rb.velocity.y < -.1f)
+            state = MovementState.falling; // moving down with speed < -.1f
+
+        anim.SetInteger("state", (int) state);
+    }
+
+    // Does player stand on the ground?
+    private bool isGrounded() { return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround); }
+}
