a837c6d8b95613cddaf0777ea1b70f9bbf200b77
Marius Vainauskis <Marius.Vainauskis@ktu.edu>
2024-02-24 14:45:18 +0200

    Player gets teleported to the last location where he was standing on the ground.

---
+++ Assets/Scripts/GameRespawn.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRespawn : MonoBehaviour
+{
+    public GroundDetection groundDetection;
+
+    [SerializeField]
+    public float threshold;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Save the initial position of the player
+        groundDetection.lastContactPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 currentPosition = transform.position;
+        Vector2 lastContactPosition = groundDetection.lastContactPosition;
+
+        float distance = Vector2.Distance(currentPosition, lastContactPosition);
+
+        Debug.Log("Distance from last contact position: " + distance);
+
+        if (distance >= threshold)
+        {
+            // Respawn the player at the initial position
+            Vector2 respawnPosition = groundDetection.lastContactPosition;
+            respawnPosition.y = groundDetection.lastContactPosition.y + 1;
+            transform.position = respawnPosition;
+            Debug.Log("Player teleported to: " + groundDetection.lastContactPosition);
+        }
+    }
+}
