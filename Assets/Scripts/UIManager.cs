d35868dfc772d07b93251fc7ce215a868415e098
Julius <60598199+jultam@users.noreply.github.com>
2024-03-11 20:24:53 +0200

    Hearts indicator added

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -10,22 +10,48 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager uiManager { get; private set; }
 
-    [SerializeField] private TextMeshProUGUI _healthText;
     [SerializeField] private TextMeshProUGUI _uiText;
+
+    private int maxHP;
+    [SerializeField] GameObject heartContainer;
+    [SerializeField] GameObject heartPrefab;
+    [SerializeField] GameObject lostHeartPrefab;
 
     void Start()
     {
+        maxHP = GameManager.gameManager._playerHealth.MaxHealth;
         updateHealth();
     }
 
     public void updateHealth()
     {
-        _healthText.text = "health: " + GameManager.gameManager._playerHealth.Health;
+        DrawHeart(GameManager.gameManager._playerHealth.Health, maxHP);
     }
 
     public void updateExpPoints()
     {
         _uiText.text = "exp: " + GameManager.gameManager._playerExpPoints.ExpPoints;
+    }
+
+    public void DrawHeart (int hearts, int maxHearts)
+    {
+        Transform containerTrans = heartContainer.transform;
+        foreach (Transform child in containerTrans) {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < maxHearts; i++)
+        {
+
+            if (i + 1 <= hearts)
+            {
+                GameObject heart = Instantiate(heartPrefab, containerTrans.position, Quaternion.identity);
+                heart.transform.SetParent(containerTrans,false);
+            } else {
+                GameObject heart = Instantiate(lostHeartPrefab, containerTrans.position, Quaternion.identity);
+                heart.transform.SetParent(containerTrans, false);
+            }
+        }
     }
 
     void Awake()
