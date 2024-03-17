a15c3ed4fd57eb770e58d2556c2831ed37c0bf47
pillarcat <buteliai16@gmail.com>
2024-03-17 11:29:52 +0200

    Added Main menu button on death

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -102,4 +102,8 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
