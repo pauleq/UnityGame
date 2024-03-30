aee3b166685c920a620e87be990c2a4eed01df8a
Marius Vainauskis <mvainauskis@gmail.com>
2024-03-30 17:50:49 +0200

    Start menu changed so player can continue from last unfinished level

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -1,13 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
+    int startLevel = 1;
+
+    private void Start()
+    {
+        GameManager.gameManager.gameSaveData = SaveSystem.LoadData();
+        Debug.Log(GameManager.gameManager.gameSaveData.ToString());
+
+        for (int i = 0; GameManager.gameManager.gameSaveData.levelFinished[i] == true; i++, startLevel++) ;
+
+        Debug.Log("Player can continue from level " + startLevel);
+
+        GameObject buttonObject = GameObject.Find("Button");
+
+        if (buttonObject != null)
+        {
+            Button button = buttonObject.GetComponent<Button>();
+
+            if (button != null)
+                button.GetComponentInChildren<Text>().text = "Continue from level " + startLevel.ToString();
+        }
+    }
+
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + startLevel);
     }
 
     public void QuitGame()
