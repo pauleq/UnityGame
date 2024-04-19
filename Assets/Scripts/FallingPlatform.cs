bec0c9de9f060cf35b306e6ce41110ed347c3791
pillarcat <buteliai16@gmail.com>
2024-04-19 10:53:32 +0300

    falling platform animation

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -7,6 +7,7 @@ public class FallingPlatform : MonoBehaviour
     private float fallDelay = 1f;
     private float destroyDelay = 2f;
 
+    [SerializeField] Animator anim;
     [SerializeField] private Rigidbody2D rb;
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -19,6 +20,9 @@ public class FallingPlatform : MonoBehaviour
 
     private IEnumerator Fall()
     {
+        anim.SetTrigger("Start");
+        yield return new WaitForSeconds(1);
+        anim.SetTrigger("End");
         yield return new WaitForSeconds(fallDelay);
         rb.bodyType = RigidbodyType2D.Dynamic;
         Destroy(gameObject, destroyDelay);
