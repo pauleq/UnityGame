e4bde681d23bacc934223ee1766ae50f7e6fe07f
pauleq <p.reinikyte@gmail.com>
2024-04-21 12:03:08 +0300

    Fixed level transition and cannins on level 3

--- Assets/Scripts/LevelChange.cs
+++ Assets/Scripts/LevelChange.cs
@@ -48,7 +48,7 @@ public class LevelChange : MonoBehaviour
             SceneManager.LoadScene("EndStory");
 			//StartCoroutine(endOfLevel());
 		}
-		else if (SceneManager.GetActiveScene().name != "Level 1")
+		else 
 		{
 			StartCoroutine(loadlevel());
 			GameManager.gameManager.gameSaveData.UpdateSave(SceneManager.GetActiveScene().buildIndex - 1, GameManager.gameManager._playerExpPoints.ExpPoints, expCounter.CalculateStars(SceneManager.GetActiveScene().name));
