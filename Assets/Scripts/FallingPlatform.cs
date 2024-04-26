d3101e5c0042b7f366df38c540ddf6d18cd1639a
Julius <60598199+jultam@users.noreply.github.com>
2024-04-26 16:01:32 +0300

    UM-71 Level 5 platforms and falling platforms tweak

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -6,6 +6,7 @@ public class FallingPlatform : MonoBehaviour
 {
     private float fallDelay = 1f;
     private float destroyDelay = 2f;
+    public bool fallup=false;
 
     [SerializeField] Animator anim;
     [SerializeField] private Rigidbody2D rb;
@@ -25,6 +26,8 @@ public class FallingPlatform : MonoBehaviour
         anim.SetTrigger("End");
         yield return new WaitForSeconds(fallDelay);
         rb.bodyType = RigidbodyType2D.Dynamic;
+        if (fallup) rb.gravityScale = -1;
+        rb.mass = 4f;
         Destroy(gameObject, destroyDelay);
     }
 }
