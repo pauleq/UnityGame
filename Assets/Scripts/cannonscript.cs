0d709291f62de5a8098884eab179c76227696225
Marius Vainauskis <mvainauskis@gmail.com>
2024-05-10 18:26:30 +0300

    UM-94 Added sound effects for cannon bullet launch

--- Assets/Scripts/cannonscript.cs
+++ Assets/Scripts/cannonscript.cs
@@ -9,6 +9,10 @@ public class cannonscript : MonoBehaviour
     float timebetween;
     public float starttimebetween;
 
+    [SerializeField] private AudioSource bulletLaunchSoundEffect;
+    public Transform player;
+    [SerializeField] private float proximityDistance = 25f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +25,11 @@ public class cannonscript : MonoBehaviour
         if (timebetween <= 0)
         {
             Instantiate(bullet, firepoint.position, firepoint.rotation);
+
+            if (player != null)
+                if (Vector3.Distance(transform.position, player.position) < proximityDistance)
+                    bulletLaunchSoundEffect.Play();
+
             timebetween = starttimebetween;
         }
         else
