a5466ef95cb36ab0e359de9d9d8ea46fd8a98c5d
Julius <60598199+jultam@users.noreply.github.com>
2024-05-14 15:23:10 +0300

    adjustments

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -10,7 +10,7 @@ public class PlayerBehaviour : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.N)) 
         { 
-            PlayerTakeDamage(1);
+            PlayerTakeDamage(5);
         }
         if (Input.GetKeyDown(KeyCode.M))
         {
